68787760b78123b2b2a2bbcf524718b0233459dc
Joseado381 <josealonzo.jaa@gmail.com>
2026-05-29 21:48:00 -0300

    commit

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,5 +1,45 @@
+namespace EspacioCalculadora;
+
 public class Calculadora
 {   
-    private double Memoria;
+    private double memoria;
 
+    public double Memoria {get => memoria;}
+
+    private double resultado;
+
+    public double Resultado { get => resultado;}
+
+    public void Suma(double dato)
+    {
+        memoria += dato;
+        resultado = dato;
+    }
+
+    public void Restar(double dato)
+    {
+        memoria -= dato;
+        resultado = dato;
+    }
+
+    public void Multiplicar(double dato)
+    {
+        memoria *= dato;
+        resultado = dato;
+    }
+
+    public void Dividir(double dato)
+    {
+        if (dato != 0)
+        {
+            memoria /= dato;
+            resultado = dato;
+        }
+    }
+
+    public void Limpiar(double dato)
+    {
+        memoria = 0;
+        dato = 0;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EspacioCalculadora;
+Calculadora miCalculadora = new Calculadora();
+
+miCalculadora.Suma(10);
+
+Console.WriteLine("Resultado: "+miCalculadora.Memoria);
+
+int opcion = 0;
+// Interfaz grafica
+// despues de hacer cada operacion limpiar la memoria
+do
+{
+    System.Console.WriteLine("******Calculadora Simple******");
+    
+} while (opcion != 10);
